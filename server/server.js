a6a8463a6b642d12e33fcb6d0555096456a06774
“seventh720” <1791303902@qq.com>
2024-05-29 01:05:33 +0800

    Home finished

--- server/server.js
+++ server/server.js
@@ -11,28 +11,17 @@ const io = socketIo(server);
 
 const port = process.env.PORT || 8080;
 
-const quizDataPath = path.join(__dirname, 'data', 'quiz-data.json');
 const leaderboardPath = path.join(__dirname, 'data', 'leaderboard.json');
 
-let quizData = [];
-try {
-  const data = fs.readFileSync(quizDataPath);
-  quizData = JSON.parse(data);
-} catch (error) {
-  console.error('初始化答题数据失败，将使用空数组开始', error);
-}
-
-// 读取初始排行榜数据
 let leaderboardData = [];
 try {
   const data = fs.readFileSync(leaderboardPath);
   leaderboardData = JSON.parse(data);
   leaderboardData.sort((a, b) => b.score - a.score || a.timeTaken - b.timeTaken);
 } catch (error) {
-  console.error('初始化排行榜数据失败，将使用空数组开始', error);
+  console.error('Initializaiton failed.', error);
 }
 
-// 中间件
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -68,5 +57,5 @@ app.get('/', (req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
+  console.log(`Server is listening on http://localhost:${port}/`);
 });
